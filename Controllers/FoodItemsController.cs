fe0dd2f221af0e1ec12b39a6f2e3065d9e1d9e05
luule <ryuu208@gmail.com>
2021-01-27 15:06:29 +0700

    first commit

---
+++ Controllers/FoodItemsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FoodsOrderAPI.Data;
+using FoodsOrderAPI.Models;
+
+namespace FoodsOrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemsController : ControllerBase
+    {
+        private readonly FoodsOrderAPIContext _context;
+
+        public FoodItemsController(FoodsOrderAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FoodItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodItem>>> GetFoodItems()
+        {
+            var ListFoodItem= await _context.FoodItems.ToListAsync();
+            return Ok(ListFoodItem);
+        }
+
+        // GET: api/FoodItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FoodItem>> GetFoodItem(long id)
+        {
+            var foodItem = await _context.FoodItems.FindAsync(id);
+
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foodItem);
+        }
+
+        // PUT: api/FoodItems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFoodItem(long id, FoodItem foodItem)
+        {
+            if (id != foodItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(foodItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Content("Successfully");
+        }
+
+        // POST: api/FoodItems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<FoodItem>> PostFoodItem(FoodItem foodItem)
+        {
+            _context.FoodItems.Add(foodItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFoodItem", new { id = foodItem.Id }, foodItem);
+        }
+
+        // DELETE: api/FoodItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<FoodItem>> DeleteFoodItem(long id)
+        {
+            var foodItem = await _context.FoodItems.FindAsync(id);
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.FoodItems.Remove(foodItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(foodItem);
+        }
+
+        private bool FoodItemExists(long id)
+        {
+            return _context.FoodItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/FoodsOrderAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using FoodsOrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodsOrderAPI.Data
+{
+    public class FoodsOrderAPIContext : DbContext
+    {
+        public FoodsOrderAPIContext(DbContextOptions<FoodsOrderAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodItem> FoodItems { get; set; }
+    }
+}
